76b601d4b3ba8b1460497b3a145d2c890c284ea0
K123Ritesh <riteshkumarlsarai@gmail.com>
2024-04-17 13:09:10 +0530

    Subscription UI

--- src/components/pricing.tsx
+++ src/components/pricing.tsx
@@ -1,7 +1,16 @@
 // Pricing.tsx
 
-import { Box, Text, Stack, List, ListItem, ListIcon, Button, useColorModeValue } from '@chakra-ui/react';
-import { CheckIcon } from '@chakra-ui/icons';
+import {
+  Box,
+  Text,
+  Stack,
+  List,
+  ListItem,
+  ListIcon,
+  Button,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { CheckIcon } from "@chakra-ui/icons";
 
 interface PricingProps {
   selected: boolean;
@@ -12,56 +21,70 @@ interface PricingProps {
   price: number;
 }
 
-const Pricing: React.FC<PricingProps> = ({ price, currentType, subscriptionType, selected, onclick, onbuyNow }) => {
+const Pricing: React.FC<PricingProps> = ({
+  price,
+  currentType,
+  subscriptionType,
+  selected,
+  onclick,
+  onbuyNow,
+}) => {
   return (
     <Box
       onClick={onclick}
-      m={'3'}
-      w={'330px !important'}
-      bg={selected ? 'blue' : 'white'}
-      boxShadow={selected ? '3xl' : 'xl'}
-      rounded={'md'}
-      height={'450px !important'}
-      overflow={'hidden'} >
+      m={"3"}
+      w={"330px !important"}
+      bg={selected ? "blue" : "white"}
+      boxShadow={selected ? "3xl" : "xl"}
+      rounded={"md"}
+      height={"450px !important"}
+      overflow={"hidden"}
+    >
       <Stack
-        textAlign={'center'}
+        textAlign={"center"}
         p={6}
-        color={useColorModeValue('gray.800', 'white')}
-        align={'center'}>
+        color={useColorModeValue("gray.800", "white")}
+        align={"center"}
+      >
         <Text
-          fontSize={'sm'}
+          fontSize={"sm"}
           fontWeight={500}
-          bg={useColorModeValue('green.50', 'green.900')}
+          bg={useColorModeValue("green.50", "green.900")}
           p={2}
           px={3}
-          color={'green.500'}
-          rounded={'full'}>
+          color={"green.500"}
+          rounded={"full"}
+        >
           {subscriptionType}
         </Text>
-        <Stack direction={'row'} align={'center'} justify={'center'}>
-          <Text fontSize={'3xl'}>$</Text>
-          <Text fontSize={'6xl'} fontWeight={800} color={selected ? 'white' : 'blue'}>
+        <Stack direction={"row"} align={"center"} justify={"center"}>
+          <Text fontSize={"3xl"}>$</Text>
+          <Text
+            fontSize={"6xl"}
+            fontWeight={800}
+            color={selected ? "white" : "blue"}
+          >
             {price}
           </Text>
-          <Text color={selected ? 'white' : 'grey.500'}>/month</Text>
+          <Text color={selected ? "white" : "grey.500"}>/month</Text>
         </Stack>
       </Stack>
 
-      <Box bg={selected ? 'blue' : 'white'} px={6} py={10}>
+      <Box bg={selected ? "blue" : "white"} px={6} py={10}>
         <List spacing={3}>
-          <ListItem color={selected ? 'white' : 'blue'}>
+          <ListItem color={selected ? "white" : "blue"}>
             <ListIcon as={CheckIcon} color="green.400" />
             5.000 page views
           </ListItem>
-          <ListItem color={selected ? 'white' : 'blue'}>
+          <ListItem color={selected ? "white" : "blue"}>
             <ListIcon as={CheckIcon} color="green.400" />
             50 automation executions
           </ListItem>
-          <ListItem color={selected ? 'white' : 'blue'}>
+          <ListItem color={selected ? "white" : "blue"}>
             <ListIcon as={CheckIcon} color="green.400" />
             50 identified users
           </ListItem>
-          <ListItem color={selected ? 'white' : 'blue'}>
+          <ListItem color={selected ? "white" : "blue"}>
             <ListIcon as={CheckIcon} color="green.400" />
             All features
           </ListItem>
@@ -69,24 +92,25 @@ const Pricing: React.FC<PricingProps> = ({ price, currentType, subscriptionType,
 
         <Button
           mt={10}
-          w={'full'}
-          bg={'green.400'}
-          color={'white'}
-          rounded={'xl'}
-          boxShadow={'0 5px 20px 0px rgb(72 187 120 / 43%)'}
+          w={"full"}
+          bg={"green.400"}
+          color={"white"}
+          rounded={"xl"}
+          boxShadow={"0 5px 20px 0px rgb(72 187 120 / 43%)"}
           _hover={{
-            bg: 'green.500',
+            bg: "green.500",
           }}
           _focus={{
-            bg: 'green.500',
+            bg: "green.500",
           }}
           onClick={onbuyNow}
         >
-          {currentType == subscriptionType ? 'Renew Now' : 'Upgrade to ' + subscriptionType}
+          {currentType == subscriptionType
+            ? "Renew Now"
+            : "Upgrade to " + subscriptionType}
         </Button>
       </Box>
     </Box>
-
   );
 };
 
