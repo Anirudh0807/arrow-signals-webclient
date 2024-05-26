f29119082d5c7117d9979500b8175b304a8946bb
Anirudh J <anirudhjaishankar97@gmail.com>
2024-05-26 19:55:13 +0530

    Set font to poppins

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,10 +1,17 @@
-import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
+import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
+import "@fontsource/poppins";
 
 const config: ThemeConfig = {
-  initialColorMode: 'dark',
+  initialColorMode: "dark",
   useSystemColorMode: false,
-}
+};
 
-const theme = extendTheme({ config })
+const theme = extendTheme({
+  config,
+  fonts: {
+    heading: `'poppins', sans-serif`,
+    body: `'poppins', sans-serif`,
+  },
+});
 
-export default theme
+export default theme;
