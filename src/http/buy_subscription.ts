3ed6a1acdbd9cd8fb3b6b7929423600fe18dc637
Anirudh J <anirudhjaishankar97@gmail.com>
2024-05-27 00:27:21 +0530

    update formating in http service

--- src/http/buy_subscription.ts
+++ src/http/buy_subscription.ts
@@ -1,30 +1,30 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosResponse } from "axios";
 
-const endpointUrl = 'http://localhost:3000/subscription/';
+const endpointUrl = "http://localhost:3000/subscription/";
 
-export async function upgradePremium(userId:number,subscriptionType:string){
-    
-    axios.post(endpointUrl+'upgrade/'+userId+'/'+subscriptionType, {
-        "Content-Type": "application/json"
-      })
+export async function upgradePremium(userId: number, subscriptionType: string) {
+  axios
+    .post(endpointUrl + "upgrade/" + userId + "/" + subscriptionType, {
+      "Content-Type": "application/json",
+    })
     .then((response: AxiosResponse) => {
-      console.log('Response:', response.data);
-      getDetailsOfUserSubscriptions(1)
+      console.log("Response:", response.data);
+      getDetailsOfUserSubscriptions(1);
     })
     .catch((error: unknown) => {
       // Handle error
-      console.error('Error:', error);
+      console.error("Error:", error);
     });
 }
 
-
-export async function getDetailsOfUserSubscriptions(userId:number){
-    axios.get(endpointUrl+'info/'+userId)
+export async function getDetailsOfUserSubscriptions(userId: number) {
+  axios
+    .get(endpointUrl + "info/" + userId)
     .then((response: AxiosResponse) => {
-      console.log('Response:', response.data);
+      console.log("Response:", response.data);
     })
     .catch((error: unknown) => {
       // Handle error
-      console.error('Error:', error);
+      console.error("Error:", error);
     });
 }
